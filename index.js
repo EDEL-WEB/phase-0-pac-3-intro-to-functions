f05b303a98887bdd83ed613c2612adae81a9fb86
edel <edel.omondi@student.moringaschool.com>
2025-03-02 20:03:48 +0300

    modified 1 file

--- index.js
+++ index.js
@@ -1,1 +1,97 @@
-// Follow along with the examples here
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+console.log("Hello, world!");
+function doNothing() {
+  console.log("Hello, world!");
+}
+function sayHello() {
+    console.log("Hello!");
+  }
+  sayHelloToGuadalupe();
+
+  function sayHelloToGuadalupe() {
+    console.log("Hello, Guadalupe!");
+  }
+  sayHelloToLiz();
+  function sayHelloToLiz() {
+    console.log("Hello, Liz!");
+  }
+    sayHelloToSamip();
+    function sayHelloToSamip() {
+      console.log("Hello, Samip!");
+    }
+    
+  sayHelloToGuadalupe();
+  sayHelloToLiz();
+  sayHelloToSamip();
+
+    function sayHelloToGuadalupe() {
+      console.log("Hello, Guadalupe!");
+    }
+    function sayHelloToLiz() {
+      console.log("Hello, Liz!");
+    }
+    function sayHelloToSamip() {
+      console.log("Hello, Samip!");
+    }
+    function doSomething(thing) {
+        console.log(thing);
+      }
+      function doSomething(thing) {
+        console.log(thing);
+      }
+        doSomething("Hello, Isabel!");
+        function sayHelloToIsabel() {
+          console.log("Hello, Isabel!");
+        }
+        function sayHelloTo(firstName) {
+            console.log(`Hello, ${firstName}!`);
+          }
+
+        sayHelloTo("Guadalupe"); // "Hello, Guadalupe!"sayHelloTo("Jane"); // "Hello, Ja
+        sayHelloTo("Jane"); // "Hello, Jane!"
+        sayHelloTo("R2-D2"); // "Hello, R2-D2!"
+        sayHelloTo(1); // "Hello, 1!"
+
+        function say(greeting, firstName) {
+            console.log(`${greeting}, ${firstName}!`);
+          }
+        say("Goodbye", "Julio"); // "Goodbye, Julio!"
+        say("Julio", "hello"); // "Julio, hello!"
+        say("Hello", "Julio"); // "Hello, Julio!"
+        say("Julio", "Hola"); // "Julio, Hola!"
+        say("Hola", "Julio"); // "Hola, Julio!"
+        function say(greeting, firstName) {
+            console.log("firstName: ", firstName);
+            console.log("greeting: ", greeting);
+            console.log(`${greeting}, ${firstName}!`);
+          }
+          function add(x, y) {
+            return x + y;
+          }
+            console.log(add(1, 2)); // 3 
+
+            function say(greeting, firstName) {
+                return `${greeting}, ${firstName}!`;
+              }
+                console.log(say("Hello", "Liz")); // "Hello,Liz!"
+                function add(x, y) {
+                    x + y;
+                  }
+                  
+                
+                  function say(greeting, firstName) {
+                    return `${greeting}, ${firstName}!`;
+                    console.log("I was called!");
+                    }
+                    console.log(say("Howdy", "partner"));
+
+                  
